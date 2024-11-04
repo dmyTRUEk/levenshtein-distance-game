d4658f9cc731c0e32e6f99b302447ad332b48969
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-11-04 14:44:12 +0200

    feat: impl `all_actions_iter_by_gen_blocks`

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 )]
 
 #![feature(
+	gen_blocks,
 	coroutines,
 	coroutine_trait,
 	iter_from_coroutine,
@@ -589,6 +590,68 @@ impl<const A: u8> Word<A> {
 
 		actions_vec.shrink_to_fit();
 		actions_vec
+	}
+
+	fn all_actions_iter_by_gen_block(self) -> impl Iterator<Item=Action> {
+		use Action::*;
+		gen move { // edition2024 is required for this
+			let len = self.len();
+			let alphabet = Language::get_alphabet_from_lang_index(A);
+
+			#[cfg(feature = "remove")] // COMPLEXITY: L
+			for index in 0..len {
+				yield Remove { index }
+			}
+
+			#[cfg(feature = "take")] // COMPLEXITY: ~ L^2
+			for index_start in 0..len {
+				for index_end in index_start+1..len {
+					yield Take { index_start, index_end }
+				}
+			}
+
+			#[cfg(feature = "discard")] // COMPLEXITY: ~ L^2
+			for index_start in 0..len {
+				for index_end in index_start+1..len {
+					yield Discard { index_start, index_end }
+				}
+			}
+
+			#[cfg(feature = "replace")] // COMPLEXITY: L * A
+			for index in 0..len {
+				for char in alphabet.chars() {
+					if self.chars[index] == char { continue }
+					yield Replace { char, index }
+				}
+			}
+
+			#[cfg(feature = "swap")] // COMPLEXITY: ~ L^4
+			for index1s in 0..len {
+				for index1e in index1s..len {
+					for index2s in index1e+1..len {
+						for index2e in index2s..len {
+							yield Swap { index1s, index1e, index2s, index2e }
+						}
+					}
+				}
+			}
+
+			#[cfg(feature = "add")] // COMPLEXITY: (L+1) * A
+			for index in 0..=len {
+				for char in alphabet.chars() {
+					yield Add { char, index }
+				}
+			}
+
+			#[cfg(feature = "copy")] // COMPLEXITY: ~ L^3
+			for index_start in 0..len {
+				for index_end in index_start+1..len {
+					for index_insert in 0..=len {
+						yield Copy_ { index_start, index_end, index_insert }
+					}
+				}
+			}
+		}
 	}
 
 	fn dropped_at_index(&self, index: usize) -> Self {
