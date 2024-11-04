aeb1b3ff527060bc7dd3bd9866c5b82723801620
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-11-04 14:42:38 +0200

    feat: impl `all_actions_vec`

--- src/main.rs
+++ src/main.rs
@@ -525,6 +525,72 @@ impl<const A: u8> Word<A> {
 		})
 	}
 
+	fn all_actions_vec(self) -> Vec<Action> {
+		use Action::*;
+
+		let len = self.len();
+		let alphabet = Language::get_alphabet_from_lang_index(A);
+
+		let mut actions_vec = vec![];
+
+		#[cfg(feature = "remove")] // COMPLEXITY: L
+		for index in 0..len {
+			actions_vec.push(Remove { index });
+		}
+
+		#[cfg(feature = "take")] // COMPLEXITY: ~ L^2
+		for index_start in 0..len {
+			for index_end in index_start+1..len {
+				actions_vec.push(Take { index_start, index_end });
+			}
+		}
+
+		#[cfg(feature = "discard")] // COMPLEXITY: ~ L^2
+		for index_start in 0..len {
+			for index_end in index_start+1..len {
+				actions_vec.push(Discard { index_start, index_end });
+			}
+		}
+
+		#[cfg(feature = "replace")] // COMPLEXITY: L * A
+		for index in 0..len {
+			for char in alphabet.chars() {
+				if self.chars[index] == char { continue }
+				actions_vec.push(Replace { char, index });
+			}
+		}
+
+		#[cfg(feature = "swap")] // COMPLEXITY: ~ L^4
+		for index1s in 0..len {
+			for index1e in index1s..len {
+				for index2s in index1e+1..len {
+					for index2e in index2s..len {
+						actions_vec.push(Swap { index1s, index1e, index2s, index2e });
+					}
+				}
+			}
+		}
+
+		#[cfg(feature = "add")] // COMPLEXITY: (L+1) * A
+		for index in 0..=len {
+			for char in alphabet.chars() {
+				actions_vec.push(Add { char, index });
+			}
+		}
+
+		#[cfg(feature = "copy")] // COMPLEXITY: ~ L^3
+		for index_start in 0..len {
+			for index_end in index_start+1..len {
+				for index_insert in 0..=len {
+					actions_vec.push(Copy_ { index_start, index_end, index_insert });
+				}
+			}
+		}
+
+		actions_vec.shrink_to_fit();
+		actions_vec
+	}
+
 	fn dropped_at_index(&self, index: usize) -> Self {
 		let mut self_ = self.clone();
 		let _ = self_.chars.remove(index);
