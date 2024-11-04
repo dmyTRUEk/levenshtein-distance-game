c42eae6d5b31b257609fa5da00da319a23198312
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-11-04 18:36:38 +0200

    refactor: use tabs for indenting in `macros.rs`

--- src/macros.rs
+++ src/macros.rs
@@ -2,17 +2,17 @@
 
 #[macro_export]
 macro_rules! swap {
-    ($a:expr, $b:expr) => { {
-        let t = $a;
-        $a = $b;
-        $b = t;
-    } };
+	($a:expr, $b:expr) => { {
+		let t = $a;
+		$a = $b;
+		$b = t;
+	} };
 }
 
 #[macro_export]
 macro_rules! unmut {
-    ($x:tt) => {
-        let $x = $x;
-    };
+	($x:tt) => {
+		let $x = $x;
+	};
 }
 
